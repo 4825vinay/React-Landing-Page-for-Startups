a5ddf17e2ed0eae561579180855ebd5926ab410a
4825vinay <56655839+4825vinay@users.noreply.github.com>
2021-06-22 19:09:13 +0530

    Add files via upload

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import { InfoSection } from '../../components';
+import {  homeObjTwo } from './Data';
 
+function Products() {
+  return (
+    <>
+      
+      <InfoSection {...homeObjTwo} />
+    </>
+  );
+}
+
+export default Products;
