b5a0c42611aea9f48d04d62dcf17d7a9ebe87fc4
4825vinay <56655839+4825vinay@users.noreply.github.com>
2021-06-22 19:12:47 +0530

    Add files via upload

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import { InfoSection } from '../../components';
+import { homeObjOne, homeObjThree } from './Data';
 
+function SignUp() {
+  return (
+    <>
+     
+      <InfoSection {...homeObjThree} />
+    </>
+  );
+}
+
+export default SignUp;
